84a33ee18cf1182eb9d7193aa9260a44ca7b1b3b
Minzhe Zhang <S418336@swmed.org>
2016-10-01 16:14:46 -0500

    SequenceAlign.py on alignment function

--- SequenceAlign.py
+++ SequenceAlign.py
@@ -12,12 +12,106 @@ import fnmatch
 import glob
 import argparse
 
-
+#############################################
 ### Core Functions
+#############################################
+def alignSeq2count(infileNameList, outFileNameList, countFileNameList, processPool, RefLibrary, startIdx = None, stopIdx = None, test = False):
+    '''
+    Functions for parallel process multiple sequence files
+
+    :param infileNameList:
+    :param outFileNameList:
+    :param countFileNameList:
+    :param processPool:
+    :param RefLibrary:
+    :param startIdx:
+    :param stopIdx:
+    :param test:
+    :return:
+    '''
+    if len(infileNameList) != len(outFileNameList):
+        raise ValueError('Input and ouput file number do not match!')
+
+    # assemble parameters of each file for alignment function
+    argLength = len(outFileNameList)
+    align_argList = zip(infileNameList, outFileNameList, countFileNameList, [RefLibrary]*argLength, [startIdx]*argLength, [stopIdx]*argLength)
+
+    # multiprocessing
+    = processPool.map(alignArgsWrapper, align_argList)
+
+    return zip()
+
+
+def alignArgsWrapper(argList):
+    '''
+
+    :param argList:
+    :return:
+    '''
+
+    return seq2count(*argList)
+
+
+def seq2count(infilePath, outUnalignedFilePath, RefLibrary, startIdx = None, stopIdx = None, test = False):
+    '''
+
+    :param infilePath:
+    :param outUnalignedFilePath:
+    :param RefLibrary:
+    :param startIdx:
+    :param stopIdx:
+    :param test:
+    :return:
+    '''
+
+    immed_print('Analyzing %s' % infilePath)
+
+    # Recognize supported file type of input file
+    fileType = None
+    for fileExt_Type in acceptedFileType:
+        if fnmatch.fnmatch(infilePath, fileExt_Type[0]):
+            fileType = fileExt_Type[1]
+            break
+
+    if fileType == 'fqgz':
+        linesPerRead = 4
+        infile = gzip.open(infilePath)
+    elif fileType == 'fq':
+        linesPerRead = 4
+        infile = open(infilePath)
+    elif fileType == 'fa':
+        linesPerRead = 2
+        infile = open(infilePath)
+    else:
+        raise ValueError('Sequence file type not recognized!')
+
+    SeqID_dict, IDCount_dict, expectedReadLength = parseLibraryFasta(args.RefLibrary)
+
+    curRead = 0
+    numAligning = 0
+
+    with open(outUnalignedFilePath) as unaligned_f:
+        # loop through each line of input fastq file
+        for i, fastqLine in enumerate(infile):
+            if i % 4 != 1:      # only select the sequence line
+                continue
+
+            else:
+                seq = fastqLine.strip()[startIdx:stopIdx]
+
+                if i == 1 and len(seq) != expectedReadLength:
+                    raise ValueError('Trimmed read length does not match expected reference read length!')
+
+                if seq in SeqID_dict:
+                    for eachSeqID in SeqID_dict[seq]:
 
 
 
+
+
+############################################
 ### Auxiliary Functions
+############################################
 def parseSeqFileName(fileNameList):
     '''
     Function to parse and check input file name.
@@ -80,23 +174,39 @@ def parseLibraryFasta(libraryFasta):
     if len(SeqID_Dict) == 0 or len(IDCount_Dict) == 0 or readLength[0] == 0:
         raise ValueError("Couldn't parse library fasta file or no sequence detected.")
     elif max(readLength) != min(readLength):
-        raise ValueError('Library reference contains sequence with different length, please make sure all reference sequences are of the same length.')
+        raise ValueError('Library reference contains sequence with different length, make sure all reference sequences are of the same length.')
 
     return SeqID_Dict, IDCount_Dict, readLength[0]
 
 
-
+##################################################
 ### Utility Functions
+##################################################
 def immed_print(content):
     '''
     Function to force print content on the screen immediately
 
-    :param content:
+    :param content: content to print
     :return:
     '''
 
     print content
     sys.stdout.flush()
+
+
+def check_makeDir(dir_path):
+    '''
+    Function to check and make directory if not exists.
+
+    :param dir_path: path to make directory
+    :return:
+    '''
+
+    try:
+        os.makedirs(dir_path)
+    except OSError:
+        immed_print(dir_path + ' already exists!')
+        pass
 
 
 ### Set global variables
@@ -109,7 +219,7 @@ acceptedFileType = [('*.fastq.gz', 'fqgz'),
 
 TestLines = 10000
 
-curDir = os.path.dirname(os)
+curDir = os.path.dirname(os.path.realpath(__file__))        # get the dir of the current script
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Align CRISPR sequening reads to sgRNA library to get raw gene counts')
@@ -121,11 +231,11 @@ if __name__ == '__main__':
     parser.add_argument('--trim_end', type=int, help='Trim off the tail sequence after the trim_end of every reads.')
     parser.add_argument('--test', action='store_true', default=False, help='Only analyze the head line of all input files for testing the pipeline.')
 
+
     args = parser.parse_args()
-
-    ### check window #1
+    ### check inout
     print 'Start analyzing sequencing reads ......'
-
+    print args
 
     numProcessor = max(args.processor, 1)
 
@@ -133,6 +243,8 @@ if __name__ == '__main__':
     outfileBaseList = parseSeqFileName(args.SeqFileName)
     if len(outfileBaseList) == 0:
         sys.exit('Input error: sequence files not found.')
+    else:
+        print '%d sequence read files found to be analyzed.' % len(outfileBaseList)
 
     # check library reference file
     try:
@@ -144,4 +256,22 @@ if __name__ == '__main__':
 
     except ValueError as err:
         sys.exit('Input error: ' + err.args[0])
+
+    # check output path
+    unalignedDir = os.path.join(curDir, 'analysisResult', args.AnalysisName, 'unaligned_reads')
+    check_makeDir(unalignedDir)     # unaligned reads file directory
+    countDir = os.path.join(curDir, 'analysisResult', args.AnalysisName, 'seq_count')
+    check_makeDir(countDir)     # guide sequence count file directory
+
+    unalignedFileNameList = [eachOutFile + '_unaligned.fa' for eachOutFile in outfileBaseList]
+    unalignedFilePathList = [os.path.join(unalignedDir, eachUnalignedFile) for eachUnalignedFile in unalignedFileNameList]     # all unaligned read files path
+    countFileNameList = [eachOutFile + '.count' for eachOutFile in outfileBaseList]
+    countFilePathList = [os.path.join(countDir, eachCountFile) for eachCountFile in countFileNameList]      # all count files path
+
+    # pool for multiprocessing
+    pool = multiprocessing.Pool(min(len(outfileBaseList), numProcessor))
+
+    # start to align sequence to reference
+    try:
+        alignResult = anlignSeq2count()
 
